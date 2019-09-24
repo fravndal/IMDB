44a7904eddb48fbf46d205f7cb5ff04aaabefada
ravndalf <fredrik.ravndal@allegro.no>
2019-09-24 07:50:52 +0200

    Added the enities for the IMDB project

---
+++ Domain/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+
+namespace Domain
+
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<MovieGenre> MovieGenres { get; set; }
+
+        public Genre()
+        {
+            MovieGenres = new List<MovieGenre>();
+        }
+
+    }
+}
---
+++ Domain/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string ExternalId { get; set; }
+        public string Type { get; set; }
+        public string PrimaryTitle { get; set; }
+        public string OriginalTitle { get; set; }
+        public string IsAdult { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+        public int RuntimeMinutes { get; set; }
+        public List<MovieGenre> MovieGenres { get; set; }
+
+        public Movie()
+        {
+            MovieGenres = new List<MovieGenre>();
+        }
+
+        public Movie(int id, string externalId, string type, string primaryTitle, string originalTitle, string isAdult, int startYear, int endYear, int runtimeMinutes)
+        {
+            Id = id;
+            ExternalId = externalId;
+            Type = type;
+            PrimaryTitle = primaryTitle;
+            OriginalTitle = originalTitle;
+            IsAdult = isAdult;
+            StartYear = startYear;
+            EndYear = endYear;
+            RuntimeMinutes = runtimeMinutes;
+            
+        }
+    }
+
+
+}
