fed4c2ec1ec577921cf42e37ae5161ef4bebf442
ravndalf <fredrik.ravndal@allegro.no>
2019-09-24 08:21:23 +0200

    Added Empty Repository Structure

---
+++ Testing/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Linq;
+
+namespace Testing
+{
+    class Program
+    {
+        private static IMDBDbContext _context = new IMDBDbContext();
+        static void Main(string[] args)
+        {
+            /*var context = IMDBContextFactory.GetDbContext();
+
+            var movie = context.Movies.Include(x => x.MovieGenres).ThenInclude(y => y.Genre).FirstOrDefault(z => z.Id == 3);
+
+
+            Console.WriteLine(movie);*/
+
+            //GetGenres();
+            //GetGenre();
+            GetGenresWildcard();
+        }
+
+        private static void GetGenresWildcard()
+        {
+            var genresWildcard = _context.Genres.Where(g => EF.Functions.Like(g.Name, "S%")).ToList();
+            foreach(var genre in genresWildcard)
+            {
+                Console.WriteLine(genre.Name);
+            }
+        }
+
+        private static void GetGenres()
+        {
+            
+            var genres = _context.Genres.ToList();
+            foreach(var genre in genres)
+            {
+                Console.WriteLine(genre.Name);
+            }
+
+            
+        }
+        private static void GetGenre()
+        {
+            using (var context = new IMDBDbContext())
+            {
+                var genre = context.Genres.Find(2);
+                Console.WriteLine(genre.Name);
+
+            }
+        }
+    }
+}
